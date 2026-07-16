5651287097a106f037f96ba47b59fd066bf8fce1
Abdi <abdi.hussein@student.moringaschool.com>
2026-07-16 21:35:13 +0300

    create async function

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 // Write your code here!
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// creating a function to display posts 
+function displayPosts(posts){
+    const ul = document.getElementById('post-list');
+    
+    posts.forEach( post => {
+    const li = document.createElement('li')
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
+
+    h1.textContent = post.title;
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
+    });
+};
+
+// creating async function to fetch data
+async function fetchAndDisplayPosts() {
+    try {
+        const response = await fetch('https://typicode.com');
+        if (!response.ok) {
+            throw new error(`HTTP error! status: ${response.status}`);
+        }
+
+        const posts = await response.json();
+        displayPosts(posts);
+    }
+    catch (error){
+        console.log('Error fetching data');
+    }
+}
+
+fetchAndDisplayPosts();
